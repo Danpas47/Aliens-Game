c986151cb0c6a7732ff699bb9bfdc2b08f1cbb90
danila <danpas47@gmail.com>
2023-10-29 23:09:07 +0300

    Events of mouse and keyboard

--- pkg1/realization.py
+++ pkg1/realization.py
@@ -9,3 +9,14 @@ class AlienInvasion:
         pygame.init()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Запуск основного цикла игры."""
+
+        while True:
+           # Отслеживание событий клавиатуры и мыши.
+             for event in pygame.event.get():
+                 if event.type == pygame.QUIT:
+                    sys.exit()
+          #Отображение последнего прорисованного экрана.
+             pygame.display.flip()
