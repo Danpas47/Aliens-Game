4e71c74194bef611c7683cd578e15e327f34729c
danila <danpas47@gmail.com>
2023-11-01 20:38:54 +0300

    Change of surface color and making a class example

--- pkg1/realization.py
+++ pkg1/realization.py
@@ -9,6 +9,7 @@ class AlienInvasion:
         pygame.init()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Запуск основного цикла игры."""
@@ -18,5 +19,12 @@ class AlienInvasion:
              for event in pygame.event.get():
                  if event.type == pygame.QUIT:
                     sys.exit()
+             # При каждом проходе цикла перерисовывается экран.
+             self.screen.fill(self.bg_color)
           #Отображение последнего прорисованного экрана.
              pygame.display.flip()
+
+if __name__ == '__main__':
+   # Создание экземпляра и запуск игры.
+   ai = AlienInvasion()
+   ai.run_game()
