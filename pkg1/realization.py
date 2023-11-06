d8d463d778d1dc13e7f57f9ab52c0d873f75fca6
danila <danpas47@gmail.com>
2023-11-06 19:35:38 +0300

    Initialization of Ship.py

--- pkg1/realization.py
+++ pkg1/realization.py
@@ -1,29 +1,37 @@
 import sys
 import pygame
 from settings import Settings
-class AlienInvasion:
+from ship import Ship
+class AlienInvasion():
     """Класс для управления ресурсами и поведением игры."""
 
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы."""
+
         pygame.init()
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (230, 230, 230)
+        bg_color=(230,230,230)
+        self.ship = Ship(self.screen)
 
     def run_game(self):
         """Запуск основного цикла игры."""
 
         while True:
+             self._check_events()
+             self._update_screen()
+             def _check_events(self):
            # Отслеживание событий клавиатуры и мыши.
-             for event in pygame.event.get():
-                 if event.type == pygame.QUIT:
-                    sys.exit()
+                 for event in pygame.event.get():
+                     if event.type == pygame.QUIT:
+                        sys.exit()
              # При каждом проходе цикла перерисовывается экран.
-             self.screen.fill(self.settings.bg_color)
+             def _check_events(self):
+                 self.screen.fill(self.settings.bg_color)
+                 self.ship.blitme()
           #Отображение последнего прорисованного экрана.
-             pygame.display.flip()
+                 pygame.display.flip()
 
 if __name__ == '__main__':
    # Создание экземпляра и запуск игры.
